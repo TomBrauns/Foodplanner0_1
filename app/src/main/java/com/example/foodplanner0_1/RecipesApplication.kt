eee16ec1ba7e04dcb83bb23c053081d887f6ff3c
Tom <inf3621@hs-worms.de>
2022-11-20 21:13:26 +0100

    Recipes-Fragment with Recycler-View. Proportions / Spacing still scuffed but it shows bugfree ( except for the wierd colour mistake )

---
+++ app/src/main/java/com/example/foodplanner0_1/RecipesApplication.kt
@@ -0,0 +1,21 @@
+package com.example.foodplanner0_1
+
+import android.app.Application
+import android.util.Log
+import com.example.foodplanner0_1.ui.recipes.model.RecipesNavigationRepository
+import com.example.foodplanner0_1.ui.recipes.model.FileManager
+
+class RecipesApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Log.d("XXX", "XXX")
+
+        // initializing repositories
+        val localFileManager = FileManager.initialize(applicationContext)
+        RecipesNavigationRepository.initialize(localFileManager)
+    }
+
+}
+
