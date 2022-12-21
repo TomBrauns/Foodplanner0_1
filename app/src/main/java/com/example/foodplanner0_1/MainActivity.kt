93dc8858aa5234fd194767056fd42633989dc871
Tom <inf3621@hs-worms.de>
2022-12-21 20:04:59 +0100

    -Cleaned up the Recipes and Hopefully relinked the Files correctly

--- app/src/main/java/com/example/foodplanner0_1/MainActivity.kt
+++ app/src/main/java/com/example/foodplanner0_1/MainActivity.kt
@@ -2,26 +2,19 @@ package com.example.foodplanner0_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.foodplanner0_1.databinding.ActivityMainBinding
-import com.example.foodplanner0_1.ui.recipes.ui.RecipesListFragment
-import com.example.foodplanner0_1.ui.recipes.viewmodel.RecipesNavigationViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), RecipesListFragment.Callbacks
-{
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewmodel: RecipesNavigationViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        viewmodel = ViewModelProvider(this).get(RecipesNavigationViewModel::class.java)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -31,20 +24,17 @@ class MainActivity : AppCompatActivity(), RecipesListFragment.Callbacks
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_user,           // Implementation of Navbar-Element "User"
-            R.id.navigation_calender,       // Implementation of Navbar-Element "Calender"
-            R.id.navigation_recipes,        // Implementation of Navbar-Element "Recipes"
-            R.id.navigation_shoppingList))  // Implementation of Navbar-Element "shoppinglist"
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_user,           // Implementation of Navbar-Element "User"
+                R.id.navigation_calender,       // Implementation of Navbar-Element "Calender"
+                R.id.navigation_recipes,        // Implementation of Navbar-Element "Recipes"
+                R.id.navigation_shoppingList
+            )
+        )  // Implementation of Navbar-Element "shoppinglist"
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 
-    override fun onRecipesSelected(recipesId: String) {
-        val toast = Toast.makeText(
-            application, "Recipe selected.",
-            Toast.LENGTH_SHORT
-        )
-        toast.show()
-    }
+
 }
