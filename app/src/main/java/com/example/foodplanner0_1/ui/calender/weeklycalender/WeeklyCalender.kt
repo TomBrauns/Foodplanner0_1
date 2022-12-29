ef2fdb76c572a445883e702616a85d355d288f18
Iván L. Murillo <diana.aguilar5526@alumnos.udg.mx>
2022-12-29 17:47:44 -0600

    Weekly Calendar days generation working

--- app/src/main/java/com/example/foodplanner0_1/ui/calender/weeklycalender/WeeklyCalender.kt
+++ app/src/main/java/com/example/foodplanner0_1/ui/calender/weeklycalender/WeeklyCalender.kt
@@ -1,7 +1,10 @@
 package com.example.foodplanner0_1.ui.calender.weeklycalender
 
 import android.annotation.SuppressLint
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.format.DateUtils
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -19,14 +22,12 @@ import java.util.*
 class WeeklyCalender : Fragment()
 {
 
+    lateinit var daysNumber: ArrayList<TextView>
+    lateinit var calendar : Calendar
     lateinit var monthText: TextView
-    lateinit var sunNum: TextView
-    lateinit var monNum: TextView
-    lateinit var tueNum: TextView
-    lateinit var wedNum: TextView
-    lateinit var thurNum: TextView
-    lateinit var friNum: TextView
-    lateinit var satNum: TextView
+    val formatter1 = SimpleDateFormat("MMM yyyy")
+    val formatter2 = SimpleDateFormat("E.dd.MMM.yyyy")
+    val nameWeekFormatter = SimpleDateFormat("EEEE", Locale.ENGLISH)
     private lateinit var adapter: MealsAdapter
     private lateinit var recyclerView: RecyclerView
     lateinit var backDate: FloatingActionButton
@@ -47,104 +48,60 @@ class WeeklyCalender : Fragment()
 
     @SuppressLint("SimpleDateFormat")
     private fun weekController(view: View) {
+        daysNumber = ArrayList<TextView>()
+        val days = listOf(R.id.sunNum, R.id.monNum, R.id.tueNum, R.id.wedNum, R.id.thurNum, R.id.friNum, R.id.satNum)
+
+        days.forEach(){
+            daysNumber.add(view.findViewById(it))
+        }
+
         monthText = view.findViewById(R.id.monthWeek)
         backDate = view.findViewById(R.id.buttonBack)
         nextDate = view.findViewById(R.id.buttonNext)
-        sunNum = view.findViewById(R.id.sunNum)
-        monNum = view.findViewById(R.id.monNum)
-        tueNum = view.findViewById(R.id.tueNum)
-        wedNum = view.findViewById(R.id.wedNum)
-        thurNum = view.findViewById(R.id.thurNum)
-        friNum = view.findViewById(R.id.friNum)
-        satNum = view.findViewById(R.id.satNum)
 
-
-        val calendar = Calendar.getInstance();
-        val time = calendar.time
-        val formatter1 = SimpleDateFormat("MMM yyyy")
-        val formatter2 = SimpleDateFormat("E.dd.MMM.yyyy")
-        var month = formatter1.format(time)
-        val today = formatter2.format(time)
-        monthText.text = month
+        calendar = Calendar.getInstance()
+        // Calendar set to first sunday
+        while(calendar.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY){
+            calendar.add(Calendar.DAY_OF_WEEK, -1)
+        }
 
         backDate.setOnClickListener {
             calendar.add(Calendar.DATE, -7)
-            val updateDate = calendar.time
-            month = formatter1.format(updateDate)
-            monthText.text = month
-            Toast.makeText(activity, "$updateDate", Toast.LENGTH_LONG).show()
+            updateWeek()
         }
 
         nextDate.setOnClickListener {
             calendar.add(Calendar.DATE, +7)
-            val updateDate = calendar.time
-            month = formatter1.format(updateDate)
-            monthText.text = month
-            Toast.makeText(activity, "$updateDate", Toast.LENGTH_LONG).show()
+            updateWeek()
         }
 
     }
 
-    private fun fillMeals() {
-        val meal1 = DayMeal(
-            "Sunday 15",
-            "Cereal",
-            "Meat",
-            "Fruit",
-        )
-        mealsList.add(meal1)
-
-        val meal2 = DayMeal(
-            "Monday 16",
-            "Cereal",
-            "Meat",
-            "Fruit",
-        )
-        mealsList.add(meal2)
-
-        val meal3 = DayMeal(
-            "Tuesday 17",
-            "Cereal",
-            "Meat",
-            "Fruit",
-        )
-        mealsList.add(meal3)
-
-        val meal4 = DayMeal(
-            "Friday 17",
-            "Cereal",
-            "Meat",
-            "Fruit",
-        )
-        mealsList.add(meal4)
-        val meal5 = DayMeal(
-            "Friday 17",
-            "Cereal",
-            "Meat",
-            "Fruit",
-        )
-        mealsList.add(meal5)
-
-        val meal6 = DayMeal(
-            "Friday 17",
-            "Cereal",
-            "Meat",
-            "Fruit",
-        )
-        mealsList.add(meal6)
-
-        val meal7 = DayMeal(
-            "Saturday 17",
-            "Cereal",
-            "Meat",
-            "Fruit",
-        )
-        mealsList.add(meal7)
+    private fun updateWeek(){
+        val itCalendar = calendar.clone() as Calendar
+        mealsList.clear()
+        adapter.notifyItemRangeRemoved(0, 7)
+        daysNumber.forEach(){
+            it.text = itCalendar.get(Calendar.DAY_OF_MONTH).toString()
+            if(DateUtils.isToday(itCalendar.timeInMillis)){
+                it.setTypeface(null, Typeface.BOLD)
+            }else{
+                it.setTypeface(null, Typeface.NORMAL)
+            }
+            val meal = DayMeal(
+                nameWeekFormatter.format(itCalendar.time) + " " + itCalendar.get(Calendar.DAY_OF_MONTH).toString(),
+                "Breakfast","Lunch","Dinner"
+            )
+            mealsList.add(meal)
+            itCalendar.add(Calendar.DATE, 1)
+        }
+        adapter.notifyItemRangeInserted(0, 7)
+        monthText.text = formatter1.format(calendar.time)
     }
-
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        fillMeals()
+        //fillMeals()
         val layoutManager = LinearLayoutManager(context)
         recyclerView = view.findViewById(R.id.mealsRecycle)
         recyclerView.layoutManager = layoutManager
@@ -156,6 +113,8 @@ class WeeklyCalender : Fragment()
                 Toast.makeText(activity, "hi", Toast.LENGTH_LONG).show()
             }
         })
+
+        updateWeek()
     }
 
 
